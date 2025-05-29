4d8d6082cb54bf6bce8ba55ba67fd8a9cb1c50a7
jez-7 <juanemanuelzamora@gmail.com>
2025-05-29 16:11:14 -0300

    Add relationships between Task and TaskList

--- src/main/java/com/todoapp/entity/TaskList.java
+++ src/main/java/com/todoapp/entity/TaskList.java
@@ -1,9 +1,14 @@
 package com.todoapp.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,7 +25,9 @@ public class TaskList {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	private Long taskId;
+	
+	@OneToMany(mappedBy = "taskList", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Task> task = new ArrayList<>();
 	
 	
 }
